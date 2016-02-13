a304be49d269141954501b1b23dc4a0e68d48318
mlcsobrevinas <igi.sobrevinas@gmail.com>
2016-02-14 03:35:59 +0800

    added functions in converting numbers to words

--- programmingAssignment01.js
+++ programmingAssignment01.js
@@ -3,7 +3,62 @@
 ** Assign 001: Programming a Number Library 
 ** Sources: http://stackoverflow.com/questions/70161/how-to-read-values-from-numbers-written-as-words
 ** 			http://blog.cordiner.net/2010/01/02/parsing-english-numbers-with-perl/
+**			http://stackoverflow.com/questions/5529934/javascript-numbers-to-words
 */
+
+//FUNCTIONS IN CONVERTING NUMBER TO WORDS
+
+//magnitude of values
+var ones=['','one','two','three','four','five','six','seven','eight','nine'];
+var tens=['','','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+var teens=['ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+
+
+function convertMillions(num){
+    if (num >= 1000000){		//if number us greater than or equal to 1,000,000
+        return convertMillions(Math.floor(num / 1000000)) + " million " + convertThousands(num % 1000000);		//convert and divide the number to 1,000,000 and get the starting digit then add the word million and call convertThousands function and get the modulo of num and 1000000
+    }
+    else {
+        return convertThousands(num);	//if num < 1,000,000
+    }
+}
+
+function convertThousands(num){
+    if (num >= 1000){		//if num is greater than or equal to 1000
+        return convertHundreds(Math.floor(num / 1000)) + " thousand " + convertHundreds(num % 1000);		//convert and divide number to 1000 to get the starting digits then add the word thousand and call convertHundreds function and get the modulo of num and 1000
+    }
+    else{
+        return convertHundreds(num);	//if num < 1000
+    }
+}
+
+function convertHundreds(num){
+    if (num > 99){		//if num is greater than 99
+        return ones[Math.floor(num / 100)] + " hundred " + convertTens(num % 100);		
+    }
+    else{
+        return convertTens(num);
+    }
+}
+
+function convertTens(num){
+    if (num < 10){		//if num is less than 10
+		return ones[num];		//return what number is it in words inside the tens magnitude
+	}else if (num >= 10 && num < 20){		//if num is greater than 10 and less than 20
+		return teens[num-10];		
+    }else{
+        return tens[Math.floor(num / 10)] + " " + ones[num % 10];
+    }
+}
+
+function convert(num){						//main calling function
+    if (num==0){
+		return "zero";						//returns zero
+	}
+    else {
+		return convertMillions(num);		//calls the convertMillions function
+	}
+}
 
 
 //FUNCTION IN CONVERTING WORDS TO NUMBER
